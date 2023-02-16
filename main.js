0c63ec70980e7c74c3119a2f376e0ae8edb61cbf
sofiiakiryk <107884647+sofiiakiryk@users.noreply.github.com>
2023-02-16 19:53:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+for (let i = 5; i <= 15; i++) console.log(i);
+//let name1 = prompt();
+//if ((name1 = 1)) {
+//for (let i = 1; i <= 5; i++) alert("one");
+//}
+for (let i = 3; i <= 20; i++) console.log(i);
+for (let i = 10; i <= 20; i++) console.log(i);
+let n1 = prompt();
+let n2 = prompt();
+let num1 = Number(n1);
+let num2 = Number(n2);
+for (let r = num1; r <= num2; r++) alert(r);
